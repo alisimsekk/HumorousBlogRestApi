e562fb81ab863b9e29de87be456fd424efd5c810
unknown <ali.simsek035@gmail.com>
2024-01-11 23:36:07 +0300

    Project created

---
+++ src/main/java/com/alisimsek/HumorousBlog/HumorousBlogApplication.java
@@ -0,0 +1,13 @@
+package com.alisimsek.HumorousBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumorousBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumorousBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alisimsek/HumorousBlog/HumorousBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alisimsek.HumorousBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumorousBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

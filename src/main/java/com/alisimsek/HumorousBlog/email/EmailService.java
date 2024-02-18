47a7e302a842810261eb7a12f7837d9fafae250b
alisimsekk <ali.simsek035@gmail.com>
2024-02-18 14:19:09 +0300

    Activation mail body is updated

--- src/main/java/com/alisimsek/HumorousBlog/email/EmailService.java
+++ src/main/java/com/alisimsek/HumorousBlog/email/EmailService.java
@@ -1,11 +1,14 @@
 package com.alisimsek.HumorousBlog.email;
 
 import com.alisimsek.HumorousBlog.configuration.HumorousProperties;
-import com.alisimsek.HumorousBlog.entity.User;
+import com.alisimsek.HumorousBlog.shared.Messages;
 import jakarta.annotation.PostConstruct;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.util.Properties;
@@ -18,12 +21,14 @@ public class EmailService {
     private final HumorousProperties humorousProperties;
 
 
+
     //mail gönderilecek adresin konfigürasyonunun yapılması
     //Ethereal mail servisi kullanılmıştır
     @PostConstruct
     public void initialize(){
+
         mailSender.setHost(humorousProperties.getEmail().host());
-        mailSender.setPort(587);
+        mailSender.setPort(humorousProperties.getEmail().port());
         mailSender.setUsername(humorousProperties.getEmail().username());
         mailSender.setPassword(humorousProperties.getEmail().password());
 
@@ -31,14 +36,38 @@ public class EmailService {
         properties.put("mail.smtp.starttls.enable","true");
     }
 
+
+
     //Gönderilecek mail bilgilerinin generate edilmesi
     public void sendActivationMail(String mail, String activationToken) {
+        String activationEmail = """
+            <html>
+                <body>
+                    <h1>${title}</h1>
+                    <a href="${url}">${clickHere}</a>
+                </body>
+            </html>
+            """;
         var activaitonUrl = humorousProperties.getClient().host() + "/activation/" + activationToken;
-        SimpleMailMessage message =new SimpleMailMessage();
-        message.setFrom(humorousProperties.getEmail().from());
-        message.setTo(mail);
-        message.setSubject("Account Activation");
-        message.setText(activaitonUrl);
-        this.mailSender.send(message);
+        var title = Messages.getMessageForLocale("humorous.mail.user.created.title", LocaleContextHolder.getLocale());
+        var clickHere = Messages.getMessageForLocale("humorous.mail.click.here",LocaleContextHolder.getLocale());
+
+        var mailBody = activationEmail
+                .replace("${url}",activaitonUrl)
+                .replace("${title}",title)
+                .replace("${clickHere}",clickHere);
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper message = new MimeMessageHelper(mimeMessage,"UTF-8");
+
+        try {
+            message.setFrom(humorousProperties.getEmail().from());
+            message.setTo(mail);
+            message.setSubject(title);
+            message.setText(mailBody,true);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+        this.mailSender.send(mimeMessage);
     }
 }

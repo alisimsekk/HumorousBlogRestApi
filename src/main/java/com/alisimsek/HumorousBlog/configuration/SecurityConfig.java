3bdff04a7ab9b705a532fd5cef0c9379a668f0ab
alisimsekk <ali.simsek035@gmail.com>
2024-04-19 23:46:53 +0300

    Basic authentication is added

--- src/main/java/com/alisimsek/HumorousBlog/configuration/SecurityConfig.java
+++ src/main/java/com/alisimsek/HumorousBlog/configuration/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,7 +13,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
